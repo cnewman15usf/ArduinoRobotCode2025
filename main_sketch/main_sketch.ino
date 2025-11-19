d7cd4a025aec1d5395459e01fcca71d6131e1be6
cnewman15usf <cnewman15@usf.edu>
2025-11-19 13:28:17 -0500

    General Bug Fixes and Updated Motor Control
    Defined special functions for controlling motors.  Forward(), Backward(), Left() Right(). Fixed General bugs in the code causing not to compile.

--- main_sketch/main_sketch.ino
+++ main_sketch/main_sketch.ino
@@ -1,12 +1,50 @@
-const int TRIG_PIN = 12; // Set the Trigger Pin
-const int ECHO_PIN = 11; // Set the Echo Pin
+const int TRIG_PIN = 7; // Set the Trigger Pin
+const int ECHO_PIN = 8; // Set the Echo Pin
 int in1 = 4; // variable names for the L298N
 int in2 = 5; // variable names for the L298N
 int in3 = 9; // variable names for the L298N
 int in4 = 10; // variable names for the L298N
 
+bool Forward() {
+      digitalWrite(in1, HIGH); // Pin High
+      digitalWrite(in2, LOW); // Pin Low
+      digitalWrite(in3, HIGH); // Pin High
+      digitalWrite(in4, LOW); // Pin Low
+      return true;
+}
 
-void Ping() { // Ping Ultrasound
+bool Backwards() {
+  digitalWrite(in1, LOW); // Pin Low
+      digitalWrite(in2, HIGH); // Pin High
+      digitalWrite(in3, LOW); // Pin Low
+      digitalWrite(in4, HIGH); // Pin High
+      return true;
+}
+
+bool Stop() {
+      digitalWrite(in1, LOW); // Pin Low
+      digitalWrite(in2, LOW); // Pin Low
+      digitalWrite(in3, LOW); // Pin Low
+      digitalWrite(in4, LOW); // Pin Low
+      return true;
+}
+
+bool Left() {
+      digitalWrite(in1, HIGH); // Pin High
+      digitalWrite(in2, LOW); // Pin Low
+      digitalWrite(in3, LOW); // Pin Low
+      digitalWrite(in4, HIGH); // Pin High
+      return true;
+}
+
+bool Right() {
+      digitalWrite(in1, LOW); // Pin Low
+      digitalWrite(in2, HIGH); // Pin High
+      digitalWrite(in3, HIGH); // Pin High
+      digitalWrite(in4, LOW); // Pin Low
+      return true;
+}
+int Ping() { // Ping Ultrasound
 digitalWrite(TRIG_PIN, LOW); // Clear Trigger Pin
 delay(2); // Wait 2 milliseconds
 digitalWrite(TRIG_PIN, HIGH); // Power Trigger Pin
@@ -16,8 +54,11 @@ digitalWrite(TRIG_PIN, LOW); // Depower the trigger pin
 return pulseIn(ECHO_PIN, HIGH); // Return the pulse from Ultra Sound Sensor
 }
 
-void ReadUltraSound() { 
-return (Ping() * 0.034) / 2; // Measure the distance and return 
+float ReadUltraSound() { 
+
+  int Distance = Ping();
+  
+return (Distance * 0.034) / 2; // Measure the distance and return 
 }
 
 void setup() {
@@ -33,32 +74,20 @@ void setup() {
 void loop() {
   const float Distance_cm = ReadUltraSound(); // Call the ReadUtltraSound Function
 
-  if (distance_cm <= 15) { // Read the data. If distance is less than or equal to 15
-    Serial.println("Motors Spinning Backwards"); // Move Motors Backwards
-    digitalWrite(in1, LOW); // Set Pin Low
-    digitalWrite(in2, HIGH); // Set Pin HIGH
-    digitalWrite(in3, LOW); // Set Pin Low
-    digitalWrite(in4, HIGH); // Set Pin High
+  if (Distance_cm <= 15) { // Read the data. If distance is less than or equal to 15
+    
+   Forward();
   }else if (Distance_cm <= 30) // If distance is less than or equal to 30 cm
   {
-    digitalWrite(in1, LOW); // Set Pin Low
-    digitalWrite(in2, LOW); // Set Pin HIGH
-    digitalWrite(in3, LOW); // Set Pin Low
-    digitalWrite(in4, LOW); // Set Pin High
+      Stop();
      Serial.println("Motors not Spinning"); // Stay in Place
   }else if (Distance_cm <= 45) // If distance is less than or equal to 45 cm
   {
-      digitalWrite(in1, HIGH); // Pin High
-      digitalWrite(in2, LOW); // Pin Low
-      digitalWrite(in3, HIGH); // Pin High
-      digitalWrite(in4, LOW); // Pin Low
+      Forward();
       Serial.println("Motors Spinning Forward!"); // Move Motors Forward
   }else // No Obj Detected
   {
-    digitalWrite(in1, LOW); // Set Pin Low
-    digitalWrite(in2, LOW); // Set Pin HIGH
-    digitalWrite(in3, LOW); // Set Pin Low
-    digitalWrite(in4, LOW); // Set Pin High
+    Stop();
     Serial.println("Motors not Spinning"); // Stay in place
   }
   Serial.println("Distance: "); // Print Distance:
